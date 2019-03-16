ef60edad9826688d8f67f1db894253456bde31cd
Noah Lubko <nlubko@gmail.com>
2019-03-16 16:28:05 +0200

    add universal

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-const express = require('express');
-const app = express();
 const port = process.env.PORT || 5000;
+    
+const server = require('./dist/server');
 
-app.use(express.static(__dirname + '/dist/top-ten-online-casinos'));
-
-//path location strategy
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/top-ten-online-casinos/index.html'));
-});
-
-app.listen(port, () => {
+server.app.listen(port, () => {
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { NgtUniversalModule } from '@ng-toolkit/universal';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,11 +12,16 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
+  imports:[
+ CommonModule,
+NgtUniversalModule,
+ 
+ TransferHttpCacheModule,
+HttpClientModule,
+ 
+    
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
