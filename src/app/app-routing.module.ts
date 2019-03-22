d2ea1716dc356461861ae92fcf717aeeaa6b5cba
Noah Lubko <nlubko@gmail.com>
2019-03-22 08:08:31 +0200

    add test comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { pages } from '@core/consts';
+
+import { TestComponent } from '@pages/test/test.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/test', pathMatch: 'full' },
+  { path: pages.test, component: TestComponent },
+  { path: '**', component: TestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './pages/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     CommonModule,
