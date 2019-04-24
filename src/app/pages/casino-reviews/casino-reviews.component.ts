32cb1ef0a62cad1bf0387e526af6ddb534d6bc30
Noah Lubko <nlubko@gmail.com>
2019-04-24 16:19:08 +0300

    add showMoreReviews

--- src/app/pages/casino-reviews/casino-reviews.component.ts
+++ src/app/pages/casino-reviews/casino-reviews.component.ts
@@ -9,68 +9,66 @@ import { Review } from '@core/interface';
   styleUrls: ['./casino-reviews.component.css']
 })
 export class CasinoReviewsComponent implements OnInit {
-  reviews: Review[] = [
-    {
-      casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
+  isShowMoreRevies: boolean;
+  _reviews: Review[] = [{
+    casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
   online casino has more than 500 games and a wide range of other great features. 32Red Casino
   is dedicated to providing high quality customer service with highly trained customer agents
   who are polite and ready to assist with all manner of queries.The gaming platform is
   reliable and the bonuses are best in class.All in all, 32Red has all of the features you
   will need to enjoy a rewarding online casino experience.`, link: ''
-    },
-    {
-      casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
+  }, {
+    casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
   online casino has more than 500 games and a wide range of other great features. 32Red Casino
   is dedicated to providing high quality customer service with highly trained customer agents
   who are polite and ready to assist with all manner of queries.The gaming platform is
   reliable and the bonuses are best in class.All in all, 32Red has all of the features you
   will need to enjoy a rewarding online casino experience.`, link: ''
-    },
-    {
-      casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
+  }, {
+    casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
   online casino has more than 500 games and a wide range of other great features. 32Red Casino
   is dedicated to providing high quality customer service with highly trained customer agents
   who are polite and ready to assist with all manner of queries.The gaming platform is
   reliable and the bonuses are best in class.All in all, 32Red has all of the features you
   will need to enjoy a rewarding online casino experience.`, link: ''
-    },
-    {
-      casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
+  }, {
+    casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
   online casino has more than 500 games and a wide range of other great features. 32Red Casino
   is dedicated to providing high quality customer service with highly trained customer agents
   who are polite and ready to assist with all manner of queries.The gaming platform is
   reliable and the bonuses are best in class.All in all, 32Red has all of the features you
   will need to enjoy a rewarding online casino experience.`, link: ''
-    },
-    {
-      casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
+  }, {
+    casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
   online casino has more than 500 games and a wide range of other great features. 32Red Casino
   is dedicated to providing high quality customer service with highly trained customer agents
   who are polite and ready to assist with all manner of queries.The gaming platform is
   reliable and the bonuses are best in class.All in all, 32Red has all of the features you
   will need to enjoy a rewarding online casino experience.`, link: ''
-    },
-    {
-      casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
+  }, {
+    casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
   online casino has more than 500 games and a wide range of other great features. 32Red Casino
   is dedicated to providing high quality customer service with highly trained customer agents
   who are polite and ready to assist with all manner of queries.The gaming platform is
   reliable and the bonuses are best in class.All in all, 32Red has all of the features you
   will need to enjoy a rewarding online casino experience.`, link: ''
-    },
-    {
-      casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
+  }, {
+    casinoName: '32Red', description: `32Red is an online casino powered by the renowned software provider, Microgaming. This
   online casino has more than 500 games and a wide range of other great features. 32Red Casino
   is dedicated to providing high quality customer service with highly trained customer agents
   who are polite and ready to assist with all manner of queries.The gaming platform is
   reliable and the bonuses are best in class.All in all, 32Red has all of the features you
   will need to enjoy a rewarding online casino experience.`, link: ''
-    }
-  ];
+  }];
+
   resizeStyle: any = {
     width: `100%`,
     height: `31px`
   };
+
+  get reviews(): Review[] {
+    return this.isShowMoreRevies ? this._reviews : this._reviews.slice(0, 5);
+  }
 
   constructor() { }
 
@@ -85,5 +83,9 @@ export class CasinoReviewsComponent implements OnInit {
       width: `${event.rectangle.width}px`,
       height: `${event.rectangle.height}px`
     };
+  }
+
+  showMoreReviews() {
+    this.isShowMoreRevies = true;
   }
 }
